371f653a12f5b9fdef69e8524ef33a7506ab2513
Sahil Kumar <54834362+Sahil4UI@users.noreply.github.com>
2020-11-08 11:55:34 +0530

    Create Operators.py

---
+++ Operators.py
@@ -0,0 +1,40 @@
+Python 3.9.0 (v3.9.0:9cf6752276, Oct  5 2020, 11:29:23) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #Operators
+>>> #Arithmetic Operators: +,-,%,/,//,**,*(mathmetical Operators)
+>>> #Relational Operator / Comparison Operator
+>>> 10 > 100
+False
+>>> 10 < 100
+True
+>>> 10 >=100
+False
+>>> 10<=100
+True
+>>> 10<=10
+True
+>>> 
+>>> 10 == 20
+False
+>>> 10 == 10
+True
+>>> #logical Operators : and , or , not
+>>> #and
+>>> 10 > 100 and 10 == 10
+False
+>>> 10>=5 and 4==4
+True
+>>> #or
+>>> 10 >=100 and 4==4
+False
+>>> 10 >=100 or 4==4
+True
+>>> #not
+>>> #True->False
+>>> #False -> True
+>>> not 10 == 100
+True
+>>> not 10 <100
+False
+>>> 

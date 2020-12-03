8b0ba07903b5fcdd868a095fc7f0bf0a29546a71
Sahil Kumar <54834362+Sahil4UI@users.noreply.github.com>
2020-12-03 20:04:25 +0530

    Add files via upload

--- Game Development/ZombieGame/pygame1.py
+++ Game Development/ZombieGame/pygame1.py
@@ -1,5 +1,6 @@
 
 import pygame
+import random
 pygame.init()
 width=1000
 height=600
@@ -9,48 +10,73 @@ white=255,255,255
 red=255,0,0
 blue=0,0,255
 
-x=0
-y=0
+
 
 target = pygame.image.load('mouseMotion.png')
 target=pygame.transform.scale(target,(70,70))
 
-bg = pygame.image.load('bg.jpg')
+bg = pygame.image.load('bg.png')
 bg =pygame.transform.scale(bg,(width,height))
 
 pygame.mouse.set_visible(False)
 
 w = target.get_width()
 h = target.get_height()
-while True:
-    gameboard.blit(bg,(0,0))
-    for event in pygame.event.get():
-        #QUIT
-        if event.type==pygame.QUIT:
-            pygame.quit()
-            quit()
-        if event.type==pygame.MOUSEMOTION:
-            x=event.pos[0]
-            y=event.pos[1]
+
+zombie1 = pygame.image.load('zombie.png')
+zombie1 = pygame.transform.scale(zombie1,(150,200))
 
 
-           
-    #pygame.draw.rect(gameboard,red,[x,y,w,h])
-    gameboard.blit(target,[x-(w/2),y-(h/2)])
+def main():
+    zombieX = random.randint(0,width-150)
+    zombieY = random.randint(100,height-300)
+    x=0
+    y=0
+    gunSound = pygame.mixer.Sound("shot_sound.wav")
     
-  
-    if x>width-w:
-        movex=-1
-    elif x<0:
-        movex=1
-    if y>height-h:
-        movey=-1
-    elif y<0:
-        movey=1
-    #pygame.draw.circle(gameboard,blue,[cx,cy],r)
+    while True:
+        gameboard.blit(bg,(0,0))
+        for event in pygame.event.get():
+            
+            #QUIT
+            if event.type==pygame.QUIT:
+                pygame.quit()
+                quit()
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                gunSound.play()
+                if rectTarget.colliderect(rectZombie):
+                        zombieX = random.randint(0,width-150)
+                        zombieY = random.randint(100,height-300)
+                    
+                
+                
+        
+                
+
+        x=pygame.mouse.get_pos()[0]
+        y=pygame.mouse.get_pos()[1]
+        gameboard.blit(zombie1,(zombieX,zombieY))
+               
+        rectZombie = pygame.Rect(zombieX,zombieY,150,200)
+        rectTarget = pygame.Rect(x,y,70,70)
+        gameboard.blit(target,[x-(w/2),y-(h/2)])
+        
+        
+      
+        if x>width-w:
+            movex=-1
+        elif x<0:
+            movex=1
+        if y>height-h:
+            movey=-1
+        elif y<0:
+            movey=1
+        #pygame.draw.circle(gameboard,blue,[cx,cy],r)
 
 
- 
+     
 
-    #flip updates only the region where changes has been made
-    pygame.display.flip()
+        #flip updates only the region where changes has been made
+        pygame.display.flip()
+main()
